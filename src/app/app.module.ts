2171a5eedb7d2739a7838f68950e787a42f25365
Jose Ignacio Santa Cruz G <jose.santacruz@solem.cl>
2017-05-05 16:53:41 -0300

    CHG Lodash dependency added and new pages
    Signed-off-by: Jose Ignacio Santa Cruz G <jose.santacruz@solem.cl>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { SwJokeProvider } from '../providers/sw-joke/sw-joke';
+import { ShyriiwookServiceProvider } from '../providers/shyriiwook-service/shyriiwook-service';
+import { LodashSampleComponent } from '../components/lodash-sample/lodash-sample';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LodashSampleComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +28,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    SwJokeProvider,
+    ShyriiwookServiceProvider
   ]
 })
 export class AppModule {}
